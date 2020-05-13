e359dc756572738f83e07ce54d04260ba0b6b848
Shaina C <scailles23@gmail.com>
2020-05-13 09:31:02 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -5,14 +5,7 @@ class Person
     @name = name
   end
 
-  def get_married(person)
-    self.partner = person
-    if person.class != Person
-      raise PartnerError
-    else
-      person.partner = self
-    end
-  end
+
  
   
   
